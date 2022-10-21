b72368f04aa6fd3312695fa9d706638ee505f5f8
ogola5 <ogolaevance5@gmail.com>
2022-10-21 07:23:08 +0300

    completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # setter method
+    def name=(name)
+        @name = name
+    end
 
+    def job=(job)
+        @job = job
+    end
+
+    #getter
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 end
